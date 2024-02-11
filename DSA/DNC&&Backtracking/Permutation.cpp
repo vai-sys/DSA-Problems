3a760c8993c20f3c83424125e376e68c94e9e95a
vaishnavi <vaishnavit582@gmail.com>
2024-02-11 23:04:19 +0530

    Backtracking

---
+++ DSA/DNC&&Backtracking/Permutation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void permutation(string s, int index) {
+    if(index == s.length() - 1) {
+        cout << s << ' ';
+        return;
+    }
+
+    for(int j = index; j < s.length(); j++) {
+        swap(s[index], s[j]);
+        permutation(s, index + 1);
+        swap(s[index], s[j]);  // Backtrack to the original string
+    }
+}
+
+int main() {
+    string s = "abc";
+    int index = 0;
+    permutation(s, index);
+
+    return 0;
+}
