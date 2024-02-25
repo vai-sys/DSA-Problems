d72780f5b79519a9ac0edf3ad5a67385d0cbd7a6
vaishnavi <vaishnavit582@gmail.com>
2024-02-25 11:59:07 +0530

    permutation

--- DSA/DNC&&Backtracking/permutation-2.cpp
+++ DSA/DNC&&Backtracking/permutation-2.cpp
@@ -1,35 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<set>
+#include<algorithm>
 using namespace std;
 
-void permutation(vector<int>& nums,int index,vector<vector<int>>& ans){
-    if(index > nums.size()){
+void permutation(vector<int>& nums, int index, set<vector<int>>& ans){
+    if(index == nums.size()){
+        ans.insert(nums);
         return;
     }
-    for(int i=index;i<nums.size();i++){
- 
-   ans.push_back(nums[i]);
-   permutation(nums,i+1,ans);
-   ans.pop_back(nums[i]);
+    for(int i = index; i < nums.size(); i++){
+        swap(nums[index], nums[i]);
+        permutation(nums, index + 1, ans);
+        swap(nums[i], nums[index]);
+    }
 }
 
+vector<vector<int>> permutationUnique(vector<int>& nums){
+   set<vector<int>> ans;
+   sort(nums.begin(), nums.end());
+   permutation(nums, 0, ans);
+   return vector<vector<int>>(ans.begin(), ans.end());
 }
 
-vector<vector<int> permutationUnique(vector<int>& nums){
-
-sort(nums.begin(),nums.end());
-vector<vector<int>> ans;
-
-permutation(nums,index,ans);
-return ans;
-
-}
 int main(){
-vector<int>nums={1,1,2};
-vector<vector<int>>result=permutationUnique(nums);
-for(auto i:result){
-    cout<<i<<endl;
-}
-return 0;
+    vector<int> nums = {1, 1, 2};
+    vector<vector<int>> result = permutationUnique(nums);
+    
+    for(auto i : result){
+        for(auto it : i){
+            cout << it << " ";
+        }
+        cout << endl;
+    }
 
+    return 0;
 }
