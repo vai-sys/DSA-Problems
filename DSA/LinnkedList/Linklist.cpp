655e5497a78c2811cafcfc21f84e2e4780c6f0c1
vaishnavi <vaishnavit582@gmail.com>
2024-02-26 18:43:31 +0530

    LL

---
+++ DSA/LinnkedList/Linklist.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+// Define a node structure
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Function to print the linked list
+void printLL(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    // Create nodes and initialize data
+    Node* first = new Node{10};
+    Node* second = new Node{20};
+    Node* third = new Node{30};
+
+    // Connect nodes to form a linked list
+    first->next = second;
+    second->next = third;
+
+    // Set head of the linked list
+    Node* head = first;
+
+    // Print the linked list
+    printLL(head);
+
+    // Deallocate memory (optional)
+    delete first;
+    delete second;
+    delete third;
+
+    return 0;
+}
