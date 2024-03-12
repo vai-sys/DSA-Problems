770ee03c93afe49f41356fb7574c7310da7be47a
vaishnavi <vaishnavit582@gmail.com>
2024-03-12 11:56:14 +0530

    Flatten linklist

--- DSA/LinnkedList/Flatten.cpp
+++ DSA/LinnkedList/Flatten.cpp
@@ -12,36 +12,33 @@ class node{
     }
 };
 
-node* merge(node* &l1,node* &l2){
-node* temp=new node(0);
-node* ans=temp;
-while(l1!=NULL && l2!=NULL){
-   if(l1->data < l2->data){
-    temp->bottom=l1;
-    temp=temp->bottom;
-    l1=l1->bottom;
-}
-else{
-    temp->bottom=l2;
-    temp=temp->bottom;
-    l2=l2->bottom;
-}
-if(l1!=NULL) return l1;
-if(l2!=NULL) return l2;
-return ans->bottom;
+node* merge(node*l1,node*l2){
+    node*res=new node(-1);
+    node*temp=res;
+    while(l1 && l2){
+        if(l1->data < l2->data){
+            temp->bottom=l1;
+            temp=temp->bottom;
+            l1=l1->bottom;
 
-}
-}
-
-node* flatten(node* &root){
-    if(root==NULL || root->next==NULL){
-        return root;
+        }
+        else{
+            temp->bottom=l2;
+            temp=temp->bottom;
+            l2=l2->bottom;
+        }
     }
-    node* temp= root->next;
-     root->next = flatten(root->next); 
-  
-        root = merge(root, root->next); 
-        return root;
+    if (l1) temp->bottom=l1;
+    if(l2) temp->bottom=l2
+
+    return res->bottom;
+
 }
 
+node* flatten(node*head){
+    if(head!=NULL && head->next!=NULL){
+        return head;
+    }
+    
+}
 
