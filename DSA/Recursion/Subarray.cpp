2c8150da9319b6be80a81ba757485b80db87a861
vaishnavi <vaishnavit582@gmail.com>
2024-06-11 14:16:36 +0530

    Subarray

--- DSA/Recursion/Subarray.cpp
+++ DSA/Recursion/Subarray.cpp
@@ -1,73 +1,29 @@
+
 #include<iostream>
 #include<vector>
 using namespace std;
-void subarray(vector<int>arr,int start, int end){
-    if(end==arr.size()){
-        return ;
+
+void subarray(vector<int>& arr, int start, int end) {
+    if (end == arr.size()) {
+        return;
     }
 
-for(int i=start;i<=end;i++){
-    cout<<arr[i]<<" ";
-}
-cout<<endl;
-subarray(arr,start,end+1);
+    for (int i = start; i <= end; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    subarray(arr, start, end + 1);
 }
 
-void printsubarray(vector<int>&nums){
-    for(int start=0;start<=nums.size();start++){
-        subarray(nums,start,start);
-
+void printsubarray(vector<int>& nums) {
+    for (int start = 0; start < nums.size(); start++) {
+        subarray(nums, start, start);
     }
 }
-int main(){
-    vector<int>nums={1,2,3,4};
-    printsubarray(nums);
-    return 0;
 
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    printsubarray(arr);
+    return 0; 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-
-// void subarray(vector<int>& arr, int start, int end) {
-//     if (end == arr.size()) {
-//         return;
-//     }
-
-//     for (int i = start; i <= end; i++) {
-//         cout << arr[i] << " ";
-//     }
-//     cout << endl;
-
-//     subarray(arr, start, end + 1);
-// }
-
-// void printsubarray(vector<int>& nums) {
-//     for (int start = 0; start < nums.size(); start++) {
-//         subarray(nums, start, start);
-//     }
-// }
-
-// int main() {
-//     vector<int> arr = {1, 2, 3, 4, 5};
-//     printsubarray(arr);
-//     return 0; 
-// }
